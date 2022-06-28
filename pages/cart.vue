809358679a65592588955a3280fa0e7aa3d1959e
Dominik Buczek <bdominoo@gmail.com>
2022-06-28 11:09:53 +0200

    build product & cart page

---
+++ pages/cart.vue
@@ -0,0 +1,192 @@
+<script setup lang="ts">
+let productQuantity = ref<number>(1);
+let errorMsg = ref<string>("");
+
+let incrementQuantity = () => {
+  productQuantity.value++;
+};
+let decrementQuantity = () => {
+  if (productQuantity.value > 1) productQuantity.value--;
+};
+</script>
+
+<template>
+  <div>
+    <div class="container mt-4 mt-md-5">
+      <h1 class="text-center fs-4 cart-header mx-auto mb-2 mb-md-0 position-relative">
+        Zawartość twojego koszyka
+      </h1>
+      <div class="d-none d-md-flex row mt-5 cart-labels">
+        <div class="col-6">
+          <h6 class="fw-light">Produkt</h6>
+        </div>
+        <div class="col-2 text-md-center">
+          <h6 class="fw-light">Cena</h6>
+        </div>
+        <div class="col-2 text-md-center">
+          <h6 class="fw-light">Ilość</h6>
+        </div>
+        <div class="col-2 text-md-center">
+          <h6 class="fw-light">Wartość</h6>
+        </div>
+      </div>
+
+      <!-- Products -->
+      <div class="row py-3">
+        <div class="col-12 col-md-6 my-auto d-flex align-items-center gap-3">
+          <i class="bi bi-x-lg fs-5 order-2 order-md-0 ms-auto ms-md-0"></i>
+          <img class="product-img" src="../assets/earrings2.jpg" />
+          <h6>Kolczyki</h6>
+        </div>
+        <div class="col-12 col-md-2 text-md-center my-auto">
+          <h6 class="d-md-none fw-light mt-2 mt-md-0">Cena</h6>
+          <h6>400zł</h6>
+        </div>
+        <div class="col-12 col-md-2 my-auto">
+          <h6 class="d-md-none fw-light mt-2 mt-md-0">Ilość</h6>
+          <div
+            class="d-flex align-items-center justify-content-md-center gap-2 product-quantity"
+          >
+            <i
+              @click="decrementQuantity"
+              class="bi bi-dash-circle"
+              :class="{
+                grey: productQuantity < 2,
+                'highlight-icon': productQuantity > 1,
+              }"
+            ></i>
+            <input v-model="productQuantity" class="quantity-input" type="number" />
+            <i @click="incrementQuantity" class="bi bi-plus-circle highlight-icon"></i>
+          </div>
+          <p v-if="errorMsg" class="error mt-1">{{ errorMsg }}</p>
+        </div>
+        <div class="col-2 my-auto">
+          <h6 class="d-md-none fw-light mt-2 mt-md-0">Wartość</h6>
+          <h6 class="text-md-center">400zł</h6>
+        </div>
+      </div>
+
+      <div class="row py-3">
+        <div class="col-12 col-md-6 my-auto d-flex align-items-center gap-3">
+          <i class="bi bi-x-lg fs-5 order-2 order-md-0 ms-auto ms-md-0"></i>
+          <img class="product-img" src="../assets/earrings.jpg" />
+          <h6>Kolczyki</h6>
+        </div>
+        <div class="col-12 col-md-2 text-md-center my-auto">
+          <h6 class="d-md-none fw-light mt-2 mt-md-0">Cena</h6>
+          <h6>400zł</h6>
+        </div>
+        <div class="col-12 col-md-2 my-auto">
+          <h6 class="d-md-none fw-light mt-2 mt-md-0">Ilość</h6>
+          <div
+            class="d-flex align-items-center justify-content-md-center gap-2 product-quantity"
+          >
+            <i
+              @click="decrementQuantity"
+              class="bi bi-dash-circle"
+              :class="{
+                grey: productQuantity < 2,
+                'highlight-icon': productQuantity > 1,
+              }"
+            ></i>
+            <input v-model="productQuantity" class="quantity-input" type="number" />
+            <i @click="incrementQuantity" class="bi bi-plus-circle highlight-icon"></i>
+          </div>
+          <p v-if="errorMsg" class="error mt-1">{{ errorMsg }}</p>
+        </div>
+        <div class="col-2 my-auto">
+          <h6 class="d-md-none fw-light mt-2 mt-md-0">Wartość</h6>
+          <h6 class="text-md-center">400zł</h6>
+        </div>
+      </div>
+
+      <!-- Summary -->
+      <div class="row mt-2 py-3">
+        <div class="col-12 col-md-4 order-2 order-md-0 mt-3 mt-md-0">
+          <NuxtLink to="/"
+            ><button class="btn btn-md btn-outline-info d-flex py-2 px-4">
+              <i class="bi bi-chevron-left me-2"></i>Kontynuuj zakupy
+            </button>
+          </NuxtLink>
+        </div>
+        <div class="col-12 col-md-4 my-auto d-flex justify-content-md-end">
+          <p class="fw-light fs-5">Razem: <span class="fw-normal">800zł</span></p>
+        </div>
+        <div class="col-12 col-md-4 mt-3 mt-md-0">
+          <NuxtLink to="#"
+            ><button class="btn btn-md btn-info py-2 px-4 text-light w-100">
+              Zamawiam<i class="bi bi-chevron-right ms-2"></i>
+            </button>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 85vh;
+}
+.cart-header {
+  max-width: 42rem;
+  text-transform: uppercase;
+}
+.cart-header:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 50%;
+  display: block;
+  width: 120px;
+  height: 2px;
+  background: #4dd3ff;
+}
+.cart-header:after {
+  content: "";
+  position: absolute;
+  right: 0;
+  top: 50%;
+  display: block;
+  width: 120px;
+  height: 2px;
+  background: #4dd3ff;
+}
+
+.cart-labels,
+.row:not(:last-child) {
+  border-bottom: 1px solid #d2d2d2;
+}
+
+.product-img {
+  width: 10rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+}
+
+.bi-x-lg {
+  transition: all 0.3s;
+  cursor: pointer;
+}
+.bi-x-lg:hover {
+  color: #ff4444;
+  cursor: pointer;
+}
+
+.quantity-input {
+  width: 100%;
+}
+
+@media screen and (max-width: 767px) {
+  .cart-header:before,
+  .cart-header:after {
+    content: none;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .quantity-input {
+    width: 3rem;
+    height: 3rem;
+  }
+}
+</style>
---
+++ pages/product/[id].vue
@@ -0,0 +1,172 @@
+<script setup lang="ts">
+let productQuantity = ref<number>(1);
+let errorMsg = ref<string>("");
+
+let incrementQuantity = () => {
+  productQuantity.value++;
+};
+let decrementQuantity = () => {
+  if (productQuantity.value > 1) productQuantity.value--;
+};
+
+const addToCart = () => {
+  errorMsg.value = "";
+  if (productQuantity.value < 1) {
+    errorMsg.value = "Wprowadź poprawną ilość";
+  }
+  // Add product to cart
+};
+</script>
+
+<template>
+  <div>
+    <div class="container mt-4">
+      <div class="d-flex gap-1 navigation">
+        <NuxtLink to="/">Strona główna</NuxtLink>
+        <i class="bi bi-chevron-right grey"></i>
+        <NuxtLink to="#">Kolczyki</NuxtLink>
+        <i class="bi bi-chevron-right grey"></i>
+        <NuxtLink to="#">Kolczyki Galaxy</NuxtLink>
+      </div>
+      <div class="row mt-4">
+        <div class="col-12 col-lg-6">
+          <img src="../../assets/earrings2.jpg" class="product-img" />
+          <div class="d-flex mt-2 gap-2 thumbnail-list">
+            <img class="product-thumbnail" src="../../assets/earrings2.jpg" />
+          </div>
+        </div>
+        <div class="col-12 col-lg-6">
+          <h2 class="mt-2 mt-lg-0">Kolczyki Galaxy</h2>
+          <p class="mt-1 fs-4">400zł</p>
+          <p class="mt-3">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus esse
+            obcaecati, neque sequi pariatur praesentium corrupti, cum amet tempore
+            repellat tenetur aliquam dignissimos reiciendis aliquid vero commodi ratione
+            id labore nobis eum quidem?
+          </p>
+          <div class="d-flex align-items-center mt-3 gap-2 product-quantity">
+            <i
+              @click="decrementQuantity"
+              class="bi bi-dash-circle"
+              :class="{
+                grey: productQuantity < 2,
+                'highlight-icon': productQuantity > 1,
+              }"
+            ></i>
+            <input v-model="productQuantity" class="quantity-input" type="number" />
+            <i @click="incrementQuantity" class="bi bi-plus-circle highlight-icon"></i>
+          </div>
+          <p v-if="errorMsg" class="error mt-1">{{ errorMsg }}</p>
+          <div class="d-flex align-items-center mt-3 gap-3">
+            <button @click="addToCart" class="btn btn-lg btn-info text-light w-100">
+              Dodaj do koszyka
+            </button>
+            <i class="bi bi-heart highlight-icon" title="Dodaj do listy życzeń"></i>
+          </div>
+          <div class="d-flex align-items-center gap-2 mt-2">
+            <i class="bi bi-truck fs-4"></i>
+            <p class="info">Wysyłka w ciągu 2 dni roboczych</p>
+          </div>
+          <div class="d-flex align-items-center gap-2">
+            <i class="bi bi-gem fs-4"></i>
+            <p class="info">Gwarancja jakości</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 85vh;
+}
+
+.product-img {
+  border-radius: 0.75rem;
+}
+
+.thumbnail-list {
+  overflow-x: auto;
+}
+
+.product-thumbnail {
+  width: 7.5rem;
+  border-radius: 0.75rem;
+  cursor: pointer;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+.navigation a {
+  transition: color 0.3s;
+  cursor: pointer;
+}
+
+.navigation a:hover {
+  color: #00bfff;
+}
+
+.info {
+  font-size: 0.9rem;
+}
+
+:global(.product-quantity i),
+.bi-heart {
+  color: #252525;
+  font-size: 1.8rem;
+  cursor: pointer;
+}
+
+:global(.highlight-icon:hover) {
+  color: #4dd3ff;
+}
+
+:global(.highlight-icon) {
+  transition: all 0.3s;
+}
+
+:global(input::-webkit-outer-spin-button),
+:global(input::-webkit-inner-spin-button) {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+:global(.btn) {
+  border-radius: 0;
+}
+
+:global(.grey) {
+  color: #afafaf !important;
+}
+
+:global(.error) {
+  color: #ff4444 !important;
+}
+
+:global(.quantity-input) {
+  width: 2.6rem;
+  height: 2.6rem;
+  border: none;
+  background-color: #eee;
+  text-align: center;
+  transition: all 0.3s;
+}
+
+:global(.quantity-input:focus) {
+  outline: none;
+  box-shadow: 0 0 4px #00bfff;
+}
+
+.quantity-input {
+  width: 100%;
+}
+
+@media screen and (min-width: 768px) {
+  .quantity-input {
+    width: 3rem;
+    height: 3rem;
+  }
+}
+</style>
