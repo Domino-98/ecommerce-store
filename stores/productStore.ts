43105565e99d22b0396f5079967c28b9a0fa84fa
Dominik Buczek <bdominoo@gmail.com>
2022-06-29 12:39:39 +0200

    build product store

--- stores/productStore.ts
+++ stores/productStore.ts
@@ -1,13 +1,38 @@
 import { defineStore } from 'pinia'
 
-export const useCounter = defineStore('counter', {
+export const useProducts = defineStore('products', {
   state: () => ({
-    
+    bestsellingProducts: [],
+    promoProducts: [],
+    wishlistProducts: [],
+    cartProducts: [],
   }),
   getters: {
-    
+    getBestsellingProducts: (state) => state.bestsellingProducts,
+    getPromoProducts: (state) => state.promoProducts,
+    getWishlistProducts: (state) => state.wishlistProducts,
+    getCartProducts: (state) => state.cartProducts,
+    getCartTotal: (state) => state.cartProducts.length === 0 ? "0" : state.cartProducts.map((product) => product.price * product.quantity).reduce((a, b) => a + b),
   },
   actions: {
-    
+    addToWishlist(product) {
+      this.wishlistProducts.push(product);
+    },
+    removeFromWishlist(id) {
+      this.wishlistProducts.splice(this.wishlistProducts.findIndex((product) => product.id === id), 1);
+    },
+    addToCart(product) {
+      this.cartProducts.push(product);
+    },
+    removeFromCart(id) {
+      this.cartProducts.splice(this.cartProducts.findIndex((product) => product.id === id), 1);
+    },
+    setBestsellingProducts(products) {
+      console.log(products);
+      this.bestsellingProducts = products;
+    },
+    setPromoProducts(products) {
+      this.promoProducts = products;
+    }
   },
 })
