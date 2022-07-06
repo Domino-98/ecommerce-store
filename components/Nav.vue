f239f2e200b533387df51dae46e075b2a7209fb1
Dominik Buczek <bdominoo@gmail.com>
2022-07-06 17:55:00 +0200

    add search & pagination

--- components/Nav.vue
+++ components/Nav.vue
@@ -9,6 +9,17 @@ setInterval(() => {
   promoIndex.value++;
   if (promoIndex.value > promoArray.value.length - 1) promoIndex.value = 0;
 }, 5000);
+
+let searchQuery = ref();
+
+const searchProducts = () => {
+  return navigateTo({
+    path: "/search",
+    query: {
+      name: searchQuery.value,
+    },
+  });
+};
 </script>
 
 <template>
@@ -23,10 +34,29 @@ setInterval(() => {
       <i @click="showTopbar = false" class="bi bi-x-lg text-white"></i>
     </div>
 
-    <nav class="navbar navbar-expand-md navbar-light bg-light position-relative">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light position-relative">
       <div class="container-fluid">
-        <a class="navbar-brand position-md-absolute logo fs-3" href="#">D&G</a>
-        <div class="d-flex gap-4 position-md-absolute ms-auto nav-icons">
+        <NuxtLink to="/" class="navbar-brand position-lg-absolute logo"
+          ><span>G&D</span></NuxtLink
+        >
+        <div
+          class="d-flex align-items-center gap-2 position-lg-absolute ms-auto nav-icons"
+        >
+          <form @submit.prevent="searchProducts" class="search">
+            <div class="search-wrapper">
+              <input
+                @keyup.enter="searchProducts"
+                v-model="searchQuery"
+                type="text"
+                placeholder="Znajdź produkt"
+                class="search-field"
+              />
+              <button
+                type="submit"
+                class="bi bi-search search-icon bg-transparent"
+              ></button>
+            </div>
+          </form>
           <!-- <i class="bi bi-person"></i> -->
           <NuxtLink to="/wishlist" title="Lista życzeń"
             ><i class="bi bi-heart"></i
@@ -45,7 +75,7 @@ setInterval(() => {
           <i class="bi bi-list text-light"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mx-auto mb-2 mb-md-0 gap-lg-3">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-lg-2">
             <li class="nav-item">
               <NuxtLink to="/" class="nav-link" active-class="active" aria-current="page"
                 ><span
@@ -107,6 +137,49 @@ setInterval(() => {
 nav {
   box-shadow: 0 4px 4px rgba(0, 0, 0, 0.05);
 }
+.search-wrapper {
+  position: relative;
+}
+.search-field {
+  width: 50px;
+  height: 50px;
+  padding: 0.35em 50px 0.35em 10px;
+  border: 1px solid transparent;
+  border-radius: 0;
+  background-color: transparent;
+  color: transparent;
+  font-family: Lato, sans-serif;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+.search-field:focus {
+  width: 40vw;
+  border-bottom-color: #4dd3ff;
+  outline: none;
+  font-family: inherit;
+  color: #2b2b2b;
+  cursor: default;
+}
+.search-field:focus ~ .search-icon {
+  background-color: transparent;
+  cursor: pointer;
+  pointer-events: auto;
+}
+.search-icon {
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border-color: transparent;
+  border-radius: 50%;
+  font-size: 1.35em;
+  text-align: center;
+  pointer-events: none;
+  transition: background-color 0.2s ease-in-out;
+}
 
 .bi-list {
   font-size: 1.6rem;
@@ -148,23 +221,52 @@ nav {
 
 .logo {
   left: 1.5rem;
+  padding: 0;
+  background-color: #000;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  background-clip: text;
+  font-family: "Rubik", sans-serif;
+  color: transparent;
+  text-shadow: 2px 2px 3px rgba(255, 255, 255, 0.3), 5px 5px 0px rgba(0, 0, 0, 0.1);
+  font-size: 2rem;
+  font-weight: 500;
+  cursor: pointer;
+  text-rendering: optimizeLegibility;
+}
+
+.logo:hover,
+.logo:focus {
+  color: transparent;
 }
 
 .nav-icons {
   right: 1.5rem;
 }
 
-.nav-icons i {
+.nav-icons i,
+.search-field + button {
   transition: all 0.3s;
   font-size: 1.5rem;
   cursor: pointer;
 }
 
-.nav-icons i:hover {
+.nav-icons i:hover,
+.search-field:hover + button {
   color: #4dd3ff;
 }
 
 .nav-link {
   padding: 0;
+}
+
+@media screen and (min-width: 992px) {
+  .search {
+    right: 2rem;
+  }
+
+  .search-field:focus {
+    width: 15vw;
+  }
 }
 </style>
--- pages/search.vue
+++ pages/search.vue
@@ -2,31 +2,28 @@
 const { find } = useStrapi4();
 const route = useRoute();
 const router = useRouter();
+let searchQuery = computed(() => route.query.name);
 let currentPage = computed(() => +route.query.page || 1);
 let pageSize = ref<number>(8);
 
-let categoryName = ref<string>();
-categoryName.value = route.params.name as string;
-let categoryCapitalized: string =
-  categoryName.value.charAt(0).toUpperCase() + categoryName.value.slice(1);
 let products = ref();
 let pagination = ref();
+let found = ref<boolean>();
 
-onMounted(async () => {
+onMounted(() => {
   fetchProducts();
 });
 
 const fetchProducts = async () => {
+  products.value = null;
+  pagination.value = null;
+
   try {
-    const response: any = await find("categories", {
-      populate: {
-        products: {
-          populate: "*",
-        },
-      } as any,
+    const response: any = await find("products", {
+      populate: "*",
       filters: {
         Name: {
-          $eq: categoryCapitalized,
+          $containsi: searchQuery.value,
         },
       },
       pagination: {
@@ -35,27 +32,34 @@ const fetchProducts = async () => {
       } as any,
     });
 
-    products.value = response.data[0].attributes.products.data;
+    products.value = response.data;
     pagination.value = response.meta.pagination;
+
     console.log(products.value);
+
+    response.data.length === 0 ? (found.value = false) : (found.value = true);
   } catch (error) {
     console.log(error);
   }
 };
 
-const changePage = (_page: number) => {
+const changePage = (page: number) => {
   router.push({
     path: "/search",
-    query: { page: _page },
+    query: { name: searchQuery.value, page: page },
   });
 };
 
+watch(searchQuery, fetchProducts);
 watch(currentPage, fetchProducts);
 </script>
 
 <template>
   <div>
-    <h3 class="text-center mt-4">{{ categoryCapitalized }}</h3>
+    <h3 class="text-center mt-4">
+      <span class="fw-light">Wynik wyszukiwania dla: </span
+      >{{ searchQuery || "Wszystkie produkty" }}
+    </h3>
     <div class="container d-flex flex-column">
       <div class="row mt-2 g-0">
         <div
@@ -78,9 +82,17 @@ watch(currentPage, fetchProducts);
         >
           <CardSkeleton />
         </div>
+
+        <div
+          v-else-if="found === false"
+          class="alert alert-info mt-4 text-center"
+          role="alert"
+        >
+          Nie znaleziono produktów z podaną nazwą
+        </div>
       </div>
 
-      <div v-if="pagination && products" class="mt-4 align-self-center">
+      <div v-if="pagination && products.length" class="mt-4 align-self-center">
         <vue-awesome-paginate
           :total-items="pagination?.total"
           :items-per-page="pageSize"
@@ -96,5 +108,32 @@ watch(currentPage, fetchProducts);
 <style scoped>
 .container {
   min-height: 85vh;
+}
+
+:global(.pagination-container) {
+  display: flex;
+  column-gap: 10px;
+}
+
+:global(.paginate-buttons) {
+  height: 40px;
+  width: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: rgb(242, 242, 242);
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(217, 217, 217);
+  color: black;
+}
+:global(.paginate-buttons:hover) {
+  background-color: #d8d8d8;
+}
+:global(.active-page) {
+  background-color: #4dd3ff;
+  border: 1px solid #4dd3ff;
+  color: white;
+}
+:global(.active-page:hover) {
+  background-color: #4dd3ff;
 }
 </style>
